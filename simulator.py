aa9d7ecde353c800fc4070d4dbdbd2be6cf48be6
arya-abhisri <104362584+arya-abhisri@users.noreply.github.com>
2022-08-06 02:09:13 +0530

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -100,7 +100,7 @@ register_values = {
 
 PC_MEMORY_ADDRESS = '00000000'
 
-
+load_store = {}
 
 
 #input the intruction :
@@ -280,9 +280,10 @@ def C(action, register_1, register_2 ):
 def D(action, register_1, memory_ad):
     global PC_MEMORY_ADDRESS
     if action =="ld":
-        register_values[register_1] = memory_ad
+	register_values[register_1] = load_store[memory_ad]
     if action == "st":
-        PC_MEMORY_ADDRESS = register_values[register_1]
+	load_store[memory_ad] = register_values[register_1]
+
 
 #type_E=["jmp","jlt", "jgt","je" ]
 def E(action, memory_ad):
