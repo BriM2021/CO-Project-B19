89ccd001f8b8d031c10a00e1feb2656dff85cbad
arya-abhisri <104362584+arya-abhisri@users.noreply.github.com>
2022-08-06 10:56:00 +0530

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -283,6 +283,7 @@ def C(action, register_1, register_2 ):
     if action == "mov" :
         if register_2 == '111':
             register_values[register_2] = int(register_values[register_1])
+            register_values['111'] = '0000000000000000'
             
         else :
             register_values[register_2] = register_values[register_1]
